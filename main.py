bb1a59500ef1a60ea983adb5c4078f822ea1bb8a
moomoocow369 <luke@beach22.com>
2025-02-16 18:03:56 -0700

    most common check

--- main.py
+++ main.py
@@ -1,11 +1,16 @@
-import re
+import ref
 
 pword = input("Enter password: ")
 nums = r'\d'
 spec = r'[^a-zA-Z0-9\s]'
 
+
 if len(pword) < 8:
     print("Password must be 8 characters or more.")
+
+if pword in open('1000-most-common-passwords.txt').read():
+    print("Password too common.")
+
 else:
     if re.search(spec, pword) and re.search(nums, pword):
         sec = 'Secure password'
